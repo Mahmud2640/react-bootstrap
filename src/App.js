f451ff352ec20da118a6e088f5c6f684ffa0782c
Mahmud2640 <mdmahmud484@gmail.com>
2022-03-29 05:13:36 +0600

    alert and accodian added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
-import './App.css';
+import "./App.css";
+import { Accordion } from "react-bootstrap";
+import AlertDismissibleExample from "./components/Alert/Alert";
+import AlertDismissible from "./components/Alert/Alert2";
+import Accodian from "./components/Accodian/Accodian";
 
 function App() {
   return (
     <div className="App">
-    <h1>Hello</h1>
+      <h1>This Is Alert</h1>
+      <AlertDismissibleExample></AlertDismissibleExample>
+      <AlertDismissible></AlertDismissible>
+      <h1>This is Accordion</h1>
+      <Accodian></Accodian>  
     </div>
   );
 }
